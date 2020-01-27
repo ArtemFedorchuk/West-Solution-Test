7534202092718927d84fda4bc8a045b18e8cabe0
ArtemFedorchuk <fedorchuk.zver@gmail.com>
2020-01-27 16:19:43 +0200

    fix style in project

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h1 style={{color: 'white',}}>this is Home</h1>
+            <h1 style={{color: 'white',}}>Home page</h1>
         </div>
     )
 };
